c068cdb422e7c96a0ae21be15d77bacacf71137b
Aleksey <alexeitcekov@abv.bg>
2019-02-21 16:25:34 +0200

    working state demo tutorial

--- examples/StateChannelsBar/state-demo-tutorial.js
+++ examples/StateChannelsBar/state-demo-tutorial.js
@@ -28,6 +28,9 @@ const responderKeyPair = {
     secretKey: '7c6e602a94f30e4ea7edabe4376314f69ba7eaa2f355ecedb339df847b6f0d80575f81ffb0a297b7725dc671da0b1769b1fc5cbe45385c7b5ad1fc2eaf1d609d'
 }
 
+const DEPOSIT_AMOUNT = 50000000000;
+const updateAmount = parseInt(DEPOSIT_AMOUNT * 0.1);
+
 const initiatorAddress = initiatorKeyPair.publicKey;
 const responderAddress = responderKeyPair.publicKey;
 
@@ -46,11 +49,11 @@ const params = {
     // Initial deposit in favour of the responder by the initiator
     pushAmount: 0,
     // Amount of tokens initiator will deposit into state channel
-    initiatorAmount: 50000000000,
+    initiatorAmount: DEPOSIT_AMOUNT,
     // Amount of tokens responder will deposit into state channel
-    responderAmount: 50000000000,
+    responderAmount: DEPOSIT_AMOUNT,
     // Minimum amount both peers need to maintain
-    channelReserve: 30000000000,
+    channelReserve: parseInt(DEPOSIT_AMOUNT / 2),
     // Minimum block height to include the channel_create_tx
     ttl: 1000,
     // Amount of blocks for disputing a solo close
@@ -69,11 +72,11 @@ createAccounts().then(() => {
         console.log('INITIATOR CHANNEL');
 
         initiatorChannel.on('statusChanged', (status) => {
-            console.log(`[${status.toUpperCase()}]`);
+            console.log(`[INITIATOR] status: [${status.toUpperCase()}]`);
         })
 
         initiatorChannel.on('onChainTx', (tx) => {
-            //console.log('==> channel_create_tx:', tx)
+            console.log('[INITIATOR] onChainTx:', tx)
         })
 
         // off chain balances
@@ -81,23 +84,24 @@ createAccounts().then(() => {
 
         initiatorChannel.sendMessage('hello world', responderAddress)
 
-        if (false) {
+        if (true) {
             initiatorChannel.update(
                 // Sender account
                 initiatorAddress,
                 // Recipient account
                 responderAddress,
                 // Amount
-                10,
+                updateAmount,
                 // This function should verify offchain transaction
                 // and sign it with initiator's private key
                 async (tx) => initiatorAccount.signTransaction(tx)
             ).then((result) => {
-                if (result.accepted) {
-                    console.log('==> Successfully transfered 10 tokens!', result)
-                } else {
-                    //console.log('=====> Transfer has been rejected')
-                }
+                console.log(`[INITIATOR] update result:`, result);
+                // if (result.accepted) {
+                //     console.log('==> Successfully transfered 10 tokens!', result)
+                // } else {
+                //     //console.log('=====> Transfer has been rejected')
+                // }
             }).catch(e => {
                 console.log('==> Error:', e);
             })
@@ -125,8 +129,16 @@ createAccounts().then(() => {
         console.log('RESPONDER CHANNEL');
 
         responderChannel.on('message', (msg) => {
-            console.log('==>Received message from:', msg)
-        })
+            console.log('[RESPONDER] ==> Received message from:', msg)
+        });
+
+        responderChannel.on('statusChanged', (status) => {
+            console.log(`[RESPONDER] status: [${status.toUpperCase()}]`);
+        });
+
+        responderChannel.on('onChainTx', (tx) => {
+            console.log('[RESPONDER] onChainTx:', tx)
+        });
 
         // close channel after a minute
         setTimeout(() => {
@@ -139,7 +151,7 @@ createAccounts().then(() => {
             }).catch(e => {
                 console.log('==> Error:', e);
             })
-        }, 15000)
+        }, 25000);
 
         responderChannel.on('error', err => console.log(err))
     }).catch(err => {
@@ -184,51 +196,41 @@ async function createAccounts() {
 }
 
 async function initiatorSign(tag, tx) {
-    console.log('==> initiatorSign:', tx);
-    if (tag === 'initiator_sign') {
+    const txData = deserializeTx(tx, true);
+    console.log('[TAG] initiatorSign:', txData.tag);
+
+    if (tag === 'initiator_sign' || txData.tag === 'CHANNEL_CREATE_TX') {
         return initiatorAccount.signTransaction(tx)
     }
 
-    const txData = deserializeTx(tx, true);
-    console.log('----> txData');
-    console.log(txData);
-
-    if (tag === 'shutdown_sign_ack') {
-        if (true || txData.tag === 'CHANNEL_CLOSE_MUTUAL_TX'
-            // To keep things simple we manually check that
-            // balances are correct (as a result of previous transfer update)
-            // txData.initiatorAmount === 49990 &&
-            // txData.responderAmount === 50010
-        ) {
-            return initiatorAccount.signTransaction(tx)
-        }
+    if (tag === 'shutdown_sign_ack' || txData.tag === 'CHANNEL_CLOSE_MUTUAL_TX') {
+        return initiatorAccount.signTransaction(tx);
     }
+
+    console.log('[INITIATOR] ==> NOT handled action');
 }
 
 async function responderSign(tag, tx) {
-    console.log('==> responderSign:', tag);
+    const txData = deserializeTx(tx);
+    console.log('[TAG] responderSign:', txData.tag);
 
-    if (tag === 'responder_sign') {
+    if (tag === 'responder_sign' || txData.tag === 'CHANNEL_CREATE_TX') {
         return responderAccount.signTransaction(tx)
     }
 
     // Deserialize binary transaction so we can inspect it
-    const txData = deserializeTx(tx);
 
     // When someone wants to transfer a tokens we will receive
     // a sign request with `update_ack` tag
-    if (tag === 'update_ack') {
-
-        // Check if update contains only one offchain transaction
-        // and sender is initiator
-        if (
-            true || txData.tag === 'CHANNEL_OFFCHAIN_TX'
-            // txData.updates.length === 1 &&
-            // txData.updates[0].from === initiatorAddress
-        ) {
-            return responderAccount.signTransaction(tx)
-        }
+    if (tag === 'update_ack'|| txData.tag === 'CHANNEL_OFFCHAIN_TX') {
+        return responderAccount.signTransaction(tx)
     }
+
+    if (tag === 'shutdown_sign_ack' || txData.tag === 'CHANNEL_CLOSE_MUTUAL_TX') {
+        return responderAccount.signTransaction(tx);
+    }
+
+    console.log('[RESPONDER] ==> NOT handled action');
 }
 
 async function connectAsInitiator(params) {
